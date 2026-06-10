9753e554db87742b5a74a63634c496ba0199f5f1
user <vinaygoudmaduri@gmail.com>
2026-06-10 15:03:55 +0530

    Bash commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -14,3 +14,33 @@ echo $a
 #variables & functions naming
 #We can use characters A-Z, a-z
 #We can use numbers & underscore
+
+# input variables
+#           < variable input >
+#   <variable input> script <variable input>
+
+
+#               env variables
+#  LHS             script          RHS
+
+
+# 1. Environment variables
+# export var=data
+
+# on command line you run export data x1=100 before exceuting the script
+echo x1-$x1
+
+# 2. LHS
+# x2=200 script
+echo x2 - $x2
+
+#the above two approaches we need to exclusively declare variable names and their data.
+#In case if we just pass the value but some variable should be assigned automatically then RHS is the approach
+
+
+# 3. rhs
+# script 300
+# here variables will be assigned automatically based on positioning
+# variable 1 - 300, can be accessed  by $1
+
+echo first argument - $1
